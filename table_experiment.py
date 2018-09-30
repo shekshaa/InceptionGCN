b10a4a9f931c589775cf61f8e79980c39e864b9a
shayan <shekshaa.75@gmail.com>
2018-09-30 15:01:42 +0330

    update to use log function in train code

--- table_experiment.py
+++ table_experiment.py
@@ -1,32 +1,26 @@
 from train import *
 
 # Table experiment for simple gcn on different localities
+is_skip_connection = FLAGS.is_skip_connection
 locality_upper_bound = 6
 adj, features, all_labels, one_hot_labels, node_weights, dense_features, num_class = load_data()
 support, placeholders = create_support_placeholder('gcn_cheby', locality_upper_bound + 1, adj, features,
                                                    one_hot_labels)
 
+# Open csv file to write average results of different locality settings
 with open('Average_accuracy.csv', mode='w') as csv_file:
     writer = csv.writer(csv_file)
+    # write header of file
     writer.writerow(['K1', 'K2', 'train_avg_acc', 'val_avg_acc', 'test_avg_acc', 'train_std_acc', 'val_std_acc',
                      'test_std_acc'])
     for l1 in range(1, locality_upper_bound + 1):
         for l2 in range(1, locality_upper_bound + 1):
             train_accuracy, val_accuracy, test_accuracy = train_k_fold('gcn_cheby', support, placeholders, features,
                                                                        all_labels, one_hot_labels, node_weights,
-                                                                       dense_features, num_class, l1, l2)
-            train_avg_acc = np.mean(train_accuracy)
-            val_avg_acc = np.mean(val_accuracy)
-            test_avg_acc = np.mean(test_accuracy)
+                                                                       dense_features, num_class,
+                                                                       is_skip_connection=is_skip_connection,
+                                                                       locality1=l1, locality2=l2)
 
-            train_std_acc = np.std(train_accuracy)
-            val_std_acc = np.std(val_accuracy)
-            test_std_acc = np.std(test_accuracy)
-
-            print('Average accuracies:')
-            print('train_avg: ', train_avg_acc, '±', train_std_acc)
-            print('val_avg: ', val_avg_acc, '±', val_std_acc)
-            print('test_avg: ', test_avg_acc, '±', test_std_acc)
-            print()
-            print()
-            writer.writerow([l1, l2, train_avg_acc, val_avg_acc, test_avg_acc, train_std_acc, val_std_acc, test_std_acc])
+            train_avg, train_std, val_avg, val_std, test_avg, test_std = avg_std_log(train_accuracy, val_accuracy,
+                                                                                     test_accuracy)
+            writer.writerow([l1, l2, train_avg, val_avg, test_avg, train_std, val_std, test_std])
